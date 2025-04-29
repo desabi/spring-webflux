f6c9d15b009306ed154df8f629e045d1023b0baf
desabi <desabi@live.com>
2025-04-29 12:50:23 -0600

    feat: demo-a, base project

---
+++ webflux-demo-a/src/main/java/com/desabisc/guide/webflux/WebfluxDemoAApplication.java
@@ -0,0 +1,13 @@
+package com.desabisc.guide.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxDemoAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxDemoAApplication.class, args);
+	}
+
+}
---
+++ webflux-demo-a/src/test/java/com/desabisc/guide/webflux/WebfluxDemoAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabisc.guide.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxDemoAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
